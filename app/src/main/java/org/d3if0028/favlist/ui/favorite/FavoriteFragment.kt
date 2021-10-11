6adc3e56803e9a2df746e93d99df9cfe20299044
Muhammad Ilham Hafizha <ilham.hafizh7@gmail.com>
2021-10-11 11:42:35 +0700

    Add Layout and Fragment

---
+++ app/src/main/java/org/d3if0028/favlist/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,7 @@
+package org.d3if0028.favlist.ui.favorite
+
+import androidx.fragment.app.Fragment
+import org.d3if0028.favlist.R
+
+class FavoriteFragment:Fragment(R.layout.fragment_favorite) {
+}
---
+++ app/src/main/java/org/d3if0028/favlist/ui/movie/MovieFragment.kt
@@ -0,0 +1,7 @@
+package org.d3if0028.favlist.ui.movie
+
+import androidx.fragment.app.Fragment
+import org.d3if0028.favlist.R
+
+class MovieFragment : Fragment(R.layout.fragment_movie) {
+}
